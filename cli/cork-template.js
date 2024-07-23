aaf6f247d977908855c0ddaa577bffc76f5c4856
Justin Merz <jrmerz@gmail.com>
2024-07-23 11:06:35 -0700

    adding directory support

--- cli/cork-template.js
+++ cli/cork-template.js
@@ -3,6 +3,7 @@
 const { program } = require('commander');
 const version = require('../package.json').version;
 const path = require('path');
+const fs = require('fs');
 const exec = require('../lib/exec');
 
 function resolve(file) {
@@ -25,6 +26,32 @@ program
     if( opts.outputFile ) resolve(opts.outputFile);
     else if( opts.outputDirectory ) opts.outputFile = path.resolve(resolve(opts.outputDirectory), path.parse(opts.templateFile).base);
     else opts.outputFile = path.resolve(process.cwd(), path.parse(opts.templateFile).base);
+
+    if( fs.statSync(opts.templateFile).isDirectory() ) {
+
+      if( !fs.existsSync(opts.outputFile) ) {
+        fs.mkdirSync(opts.outputFile, { recursive: true });
+      } else if( !fs.statSync(opts.outputFile).isDirectory() ) {
+        throw new Error('Output file must be a directory if template file is a directory');
+      }
+
+      console.log('Template file is a directory, rendering all files in directory')
+      
+      let files = fs.readdirSync(opts.templateFile);
+      let dir = opts.templateFile;
+      let outputDir = opts.outputFile;
+
+      for( let file of files ) {
+        if( fs.statSync(path.resolve(dir, file)).isDirectory() ) {
+          continue;
+        }
+        console.log(' - Rendering', file);
+        opts.templateFile = path.resolve(dir, file);
+        opts.outputFile = path.resolve(outputDir, file);
+        exec(opts.configFiles, opts.templateFile, opts.outputFile);
+      }
+      return;
+    }
     
     exec(opts.configFiles, opts.templateFile, opts.outputFile);
   })
